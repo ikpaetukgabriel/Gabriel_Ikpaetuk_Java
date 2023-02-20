768f05171ad76e1f136dbfc61d2a7483d4a69ef3
ikpaetukgabriel <gabrielikpaetuk@byui.edu>
2023-02-20 00:13:24 -0700

    add hashcode, tostring and equals to customer class

---
+++ M6-Challenge-Ikpaetuk-Gabriel/src/main/java/com/company/customerdataservice/model/Customer.java
@@ -0,0 +1,168 @@
+package com.company.customerdataservice.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Customer implements Serializable {
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String company;
+    private String phone;
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return getCustomerId() == customer.getCustomerId() &&
+                Objects.equals(getFirstName(), customer.getFirstName()) &&
+                Objects.equals(getLastName(), customer.getLastName()) &&
+                Objects.equals(getEmail(), customer.getEmail()) &&
+                Objects.equals(getCompany(), customer.getCompany()) &&
+                Objects.equals(getPhone(), customer.getPhone()) &&
+                Objects.equals(getAddress1(), customer.getAddress1()) &&
+                Objects.equals(getAddress2(), customer.getAddress2()) &&
+                Objects.equals(getCity(), customer.getCity()) &&
+                Objects.equals(getState(), customer.getState()) &&
+                Objects.equals(getPostalCode(), customer.getPostalCode()) &&
+                Objects.equals(getCountry(), customer.getCountry());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCustomerId(),
+                getFirstName(),
+                getLastName(),
+                getEmail(),
+                getCompany(),
+                getPhone(),
+                getAddress1(),
+                getAddress2(),
+                getCity(),
+                getState(),
+                getPostalCode(),
+                getCountry());
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", company='" + company + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address1='" + address1 + '\'' +
+                ", address2='" + address2 + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
